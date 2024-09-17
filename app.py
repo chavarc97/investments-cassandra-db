9a5f33b27bffa2cb1f0d6ffc65703b9a521d3f67
Salvador Rodriguez <chava.crb@gmail.com>
2024-09-17 13:49:27 -0600

    Initial project structure

---
+++ app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import logging
+import os
+import random
+# import cassandra cluster from cassandra.cluster
+from cassandra.cluster import Cluster
+
+import model
---
+++ model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import datetime
+import logging
+import random
+import uuid
+
+import time_uuid
+from cassandra.query import BatchStatement
